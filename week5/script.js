5b4ff60caab855001c6c48c07fd2b00409889599
Christopher Johnson <johnsonce@goldmail.etsu.edu>
2021-05-18 21:33:01 -0400

    JS review

---
+++ week5/script.js
@@ -0,0 +1,26 @@
+//console.log("Hello world");
+
+//let string = "hey String"
+//console.log(string);
+
+//let art = ("Anushka's favorite subject in school is art.");
+//console.log(art)
+
+//function madlib(Anushka, art) {
+//return "Anushka's favorite subject in school is art";
+//}
+
+//let art = "Anushka's favorite subject in school is art";
+
+//console.log(Anushka)
+
+//madlib('Anushka', 'art');
+//'Anushka's favorite subject in school is art.'
+
+function madlib(name, subject) {
+return (name +'s favorite subject in school is ' +subject);
+}
+
+console.log(madlib("Anushka", "art"))
+
+
